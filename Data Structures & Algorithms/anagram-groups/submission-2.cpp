67703c7b6598999e714481c2996107783ad16229
curl0z <167798247+curloz123@users.noreply.github.com>
2026-06-09 10:08:01 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if (strs.size() == 1)
+            return {{strs[0]}};
+
+        unordered_map<string, vector<string>> map;
+        for (const auto& str : strs)
+        {
+            int freq[26] = {};
+            for (auto& c : str)
+            {
+                freq[c - 'a']++;
+            }
+            std::string ch(freq, freq + 26);
+            map[ch].emplace_back(str);
+        }
+        vector<vector<string>> result;
+        for (auto& [s, v] : map)
+        {
+            result.emplace_back(v);
+        }
+        return result;
+    }
+};
