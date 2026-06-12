67cdf1a33ffdc54588cf4acb4a594c11090774cb
curl0z <167798247+curloz123@users.noreply.github.com>
2026-06-12 16:54:37 +0530

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    inline void handleIncrement(uint8_t& i, uint8_t& j, uint8_t& square)
+    {
+        ++j;
+        if (j % 3 == 0)
+        {
+            ++i;
+            j -= 3;
+            if (i % 3 == 0 && i > 0)
+            {
+                ++square;
+                if (square % 3 == 0 && square > 0)
+                {
+                    i = 0;
+                    j += 3;
+                }
+            }
+        }
+        
+    }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+        
+        std::array<std::array<bool, 9>, 9> horizontal{};
+        std::array<std::array<bool, 9>, 9> vertical{};
+        std::array<std::array<bool, 9>, 9> squares{};
+
+        uint8_t i=0, j=0, square=0;
+        while (true)
+        {
+            if (board[i][j] == '.')
+            {
+                handleIncrement(i, j, square);
+                if (square == 9)
+                    return true;
+                continue;
+            }
+            if (horizontal[i][board[i][j] - '1'])
+                return false;
+            if (vertical[j][board[i][j] - '1'])
+                return false;
+            if (squares[square][board[i][j] - '1'])
+                return false;
+            
+            horizontal[i][board[i][j] - '1'] = true;
+            vertical[j][board[i][j] - '1'] = true;
+            squares[square][board[i][j] - '1'] = true;
+
+            handleIncrement(i, j, square);
+            if (square == 9)
+                return true;
+
+        }
+    }
+};
